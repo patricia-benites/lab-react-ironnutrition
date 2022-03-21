e0c535d8fd5e579ad6cf6f50227f5036f49d1a3f
Patricia <patricia.benites.cava@gmail.com>
2022-03-21 16:41:58 +0100

    add iteration 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import './App.css';
+import { FoodBox } from './components/FoodBox/FoodBox';
 
-
-const inputFoods = require('./foods.json')
+const inputFoods = require('./foods.json');
 
 function App() {
   const [foods, setFoods] = useState(inputFoods);
@@ -10,10 +10,18 @@ function App() {
     <div className="App">
       <h1>Food List</h1>
       {foods.map((food) => {
-        return (<div key={food.id}>
-          <p> {food.name} </p>
-          <img src={food.image} width={100} />
-        </div>);
+        return (
+          <div key={food.name}>
+            <FoodBox key={food.name}
+              food={{
+                name: 'Orange',
+                calories: 85,
+                image: 'https://i.imgur.com/abKGOcv.jpg',
+                servings: 1,
+              }}
+            />
+          </div>
+        );
       })}
     </div>
   );
